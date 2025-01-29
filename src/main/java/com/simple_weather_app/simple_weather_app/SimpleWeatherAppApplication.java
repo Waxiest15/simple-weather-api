b84f3977f49f34a777858a38e21c9cf31e6b71fc
Waxiest <waxiest15@gmail.com>
2025-01-29 14:59:23 -0600

    initial setup

---
+++ src/main/java/com/simple_weather_app/simple_weather_app/SimpleWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.simple_weather_app.simple_weather_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWeatherAppApplication.class, args);
+	}
+
+}
